30c75670f2043caa619f2f26693059a8f9447848
jarvis5976 <lenoirsamuel7659@gmail.com>
2026-03-20 18:11:39 +0100

    Fix Next.js workUnitAsyncStorage error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
@@ -30,6 +29,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  experimental: {
+    reactCompiler: false,
   },
 };
 
